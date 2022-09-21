df190611932058732856f04eac1c5b8243ea0c84
marc@roosoft.com <marc@roosoft.com>
2022-09-21 08:45:45 -0400

    added iex default aliases

---
+++ .iex.exs
@@ -0,0 +1,6 @@
+alias BitcoinLib.Console
+alias BitcoinLib.Crypto
+alias BitcoinLib.Script
+alias BitcoinLib.Transaction
+alias BitcoinLib.Key.{Address, PrivateKey, PublicKey, PublicKeyHash}
+alias BitcoinLib.Key.HD.{DerivationPath, Entropy, SeedPhrase}
