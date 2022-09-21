6e21ba8c13e51ae20bf2e4a946af8c81098095e9
marc@roosoft.com <marc@roosoft.com>
2022-09-21 09:13:51 -0400

    moved Calls.GetTransaction to Calls.Blockchain.Transaction.GetTransaction

--- test/calls/blockchain/transaction/get_transaction_test.exs
+++ test/calls/blockchain/transaction/get_transaction_test.exs
@@ -1,5 +1,5 @@
-defmodule Electrum.Calls.Transaction.GetTransactionTest do
+defmodule Electrum.Calls.Blockchain.Transaction.GetTransactionTest do
   use ExUnit.Case, async: true
 
-  doctest Electrum.Calls.Transaction.GetTransaction
+  doctest Electrum.Calls.Blockchain.Transaction.GetTransaction
 end
