ad778a557d3b4f37ea9a90e86cab5821e261be94
marc@roosoft.com <marc@roosoft.com>
2022-10-17 16:13:25 -0400

    use bitcoinlib 0.2.2

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule ElectrumClient.MixProject do
     [
       {:dialyxir, "~> 1.2", only: [:dev], runtime: false},
       {:jason, "~> 1.4"},
-      {:bitcoinlib, "~> 0.2.0"}
+      {:bitcoinlib, "~> 0.2.2"}
     ]
   end
 end
